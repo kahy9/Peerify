032b56aeeb39e8ba45c3bfc6ad738d7f609d1107
Josef Kahoun <pepa.kahy@gmail.com>
2023-10-24 01:04:07 +0200

    Init shadcn/ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,32 @@
-function App() {
+import { Button } from "@/components/ui/button"
+import { ThemeProvider } from "@/components/ThemeProvider"
+import { ModeToggle } from "./components/ModeToggle/"
+import { buttonVariants } from "@/components/ui/button"
+
+const App = () => {
 
   return (
-    <>
-      <h1 className="text-3xl font-bold underline text-red-600">
-        Hello world!
-      </h1>
-    </>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <div className="container mx-auto">
+        <div className="flex justify-between">
+          <Button>Click here</Button>
+          <div className="flex items-center space-x-6 text-sm font-medium">
+            <Button asChild variant={"link"}>
+              <a href="/">Home</a>
+            </Button>
+
+            <Button asChild variant={"link"}>
+              <a href="/about">About</a>
+            </Button>
+
+            <Button asChild variant={"link"}>
+              <a href="/docs">Documentation</a>
+            </Button>
+          </div>
+          <ModeToggle />
+        </div>
+      </div>
+    </ThemeProvider>
   )
 }
 
